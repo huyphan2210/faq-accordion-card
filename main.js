51c540c7cb7fb6a9d3ae418dcce38123dd6fa820
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-02-22 14:17:51 +0700

    Handling click questions event

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 $(document).ready(() => {
-    $('.question').click(() => {
-        $(this).find('.answer').css('display', 'block')
+    $('.question').click(function() {
+        const display = $(this).find('.answer').css('display');
+        if (display === 'none') {
+            const imgTransform = $(this).find('img').css('transform');
+            $('.answer').css('display', 'none');
+            $('.questionContent').css('color', '');
+            $('.questionContent').css('font-weight', '');
+            $('img').css('transform', '');
+            $(this).find('.questionContent').css('color', 'hsl(238, 29%, 16%)');
+            $(this).find('.questionContent').css('font-weight', '700')
+            $(this).find('.answer').css('display', 'block');
+            $(this).find('img').css('transform', imgTransform + ' scaleY(-1)');
+        } else {
+            $(this).find('.questionContent').css('color', '');
+            $(this).find('.questionContent').css('font-weight', '')
+            $(this).find('.answer').css('display', 'none');
+            $(this).find('img').css('transform', '');
+        }
     })
 })
